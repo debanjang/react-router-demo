ce11a1bd3040a6ca717b45d67bc474bbbe937ca8
Debanjan Ghosh <debanjang@ymail.com>
2020-01-22 23:01:18 +0530

    Updates

---
+++ precache-manifest.b526a9b6faac54f017ca0940cfc3a9e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cacc21ee27af475cfd5f33f3653185b5",
+    "url": "/react-router-demo/index.html"
+  },
+  {
+    "revision": "d79ecdd489e3065a74ef",
+    "url": "/react-router-demo/static/css/main.2b28c9f6.chunk.css"
+  },
+  {
+    "revision": "85986aafaae3c76ad274",
+    "url": "/react-router-demo/static/js/2.27646769.chunk.js"
+  },
+  {
+    "revision": "d79ecdd489e3065a74ef",
+    "url": "/react-router-demo/static/js/main.01dacd19.chunk.js"
+  },
+  {
+    "revision": "00c954839df506636475",
+    "url": "/react-router-demo/static/js/runtime~main.67d6bcf9.js"
+  }
+]);
